92cf10a159af2eaf60fa965cbb5de2401ab26601
Mykola <radakvitka@gmail.com>
2023-08-02 08:40:21 +0300

    Subtask1-2

--- 4.HTTP/js/main.js
+++ 4.HTTP/js/main.js
@@ -1,22 +1,8 @@
-/*HTTP*/
-async function fetchUsers(apiUrl) {
-    const response = await fetch(apiUrl);
-    // waits until the request completes...
-    //console.log(JSON.stringify(data, undefined, 4));
-    return await response.json();
-}
-
-
 /*TABLE TASK*/
-async function DataTable(config) {
-    //console.log(newData["data"]);
-    //console.log(Object.keys(newData));
-    let newData = await fetchUsers(config.apiUrl);
-    const x = Object.values(newData.data).map((value) => { return value; });
-    console.log(x.length);
-    let tableStyle = `style="border:1px solid;"`;
-    let thStyle = `style="border:2px solid;background-color: rgb(4, 170, 109);"`;
-    let rowTh = ``;
+function DataTable(config, data) {
+    let tableStyle = `style="border:1px solid green"`;
+    let thStyle = `style="border:1px solid green;background-color: rgb(4, 170, 109);"`;
+    let rowTh=``;
     let tbody = `<tbody>`;
 
     // we create innerHTML code of table head
@@ -24,27 +10,25 @@ async function DataTable(config) {
         rowTh = rowTh + `<th ${thStyle}>${value.title}</th>`;
     });
     let tableData = `<thead ${thStyle}>${rowTh}</thead>`;
-    for (let i = 0; i < newData.length; i++) {
-        console.log(newData["data"][3]);
-    }
+
     //we create innerHTML code of table body
-    Object.values(newData.data).map((value) => {
-        let row = `<tr>`;
+    data.map((value, index) => {
+        let row =`<tr>`;
         delete value.id;
         Object
             .entries(value)
             .map(item => {
                 row = row + `<td ${tableStyle}>${item[1]}</td>`;
-                //  console.log(item[1]);
+                console.log(item[1]);
             })
-        row = row + `<td style="width: 90px;background-color: rgba(235,1,9,0.78);border: 2px solid;"></td></tr>`;     //here end of each row
+        row = row + `</tr>`;
         tbody = tbody + row;
     })
-    tbody = tbody + `</tbody>`;
-    // console.log(tbody);
+    tbody=tbody+`</tbody>`;
+    console.log(tbody);
 
     //finish HTML code
-    tableData = tableData + tbody;
+    tableData= tableData + tbody;
 
     //create table and append to div
     const ourConfigDiv = document.querySelector(`${config.parent}`);
@@ -55,12 +39,11 @@ async function DataTable(config) {
 
     //add style to table
     table.innerHTML = tableData;
-    table.style.width = '1000px';
+    table.style.width = '800px';
     table.style.height = '200px';
     table.style.border = '1px solid black';
     table.style.margin = `33px`;
 }
-
 
 /*TASK DATA*/
 
@@ -70,10 +53,7 @@ const config1 = {
         {title: 'Ім’я', value: 'name'},
         {title: 'Прізвище', value: 'surname'},
         {title: 'Вік', value: 'age'},
-        {title: 'Дата народження', value: 'age'},
-        {title: 'Дії', value: 'delete'},
-    ],
-    apiUrl: "https://mock-api.shpp.me/mmykola/users"
+    ]
 };
 
 const users = [
@@ -84,6 +64,4 @@ const users = [
     {id: 30051, name: 'Вася', surname: 'Васечкін', age: 15},
 ];
 
-(async function () {
-    await DataTable(config1);
-})();
+DataTable(config1, users);
